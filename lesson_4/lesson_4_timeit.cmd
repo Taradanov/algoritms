379558bfcacb44f8cad0d365e92761422b6fad49
nikolai taradanov <n.taradanov@gmail.com>
2021-12-08 16:21:30 +0700

    lesson_4

---
+++ lesson_4/lesson_4_timeit.cmd
@@ -0,0 +1,4 @@
+C:\Users\Taradanov.NV\AppData\Local\Programs\Python\Python39\python.exe -m timeit -n 100 -s "import lesson_4"
+C:\Users\Taradanov.NV\PycharmProjects\algoritms\lesson_4> python -m timeit -n 1000 -s "import lesson_4_fibo" "lesson_4_fibo.fib(20)"
+
+pause(10)
